62a402ec9c8ead8e49223c218d99a59c498f1405
unknown <sandeepshinde630@gmail.com>
2021-08-05 17:39:43 +0530

    feat:Added Switch Case

--- Program.cs
+++ Program.cs
@@ -11,7 +11,20 @@ namespace SnakeAndLadder
             Random rand = new Random();
             int DiceRoll = rand.Next(1, 7);
             Console.WriteLine(DiceRoll);
+            int PositionCheck = rand.Next(0, 3);
+            switch (PositionCheck)
+            {
+                case 0:
+                    Console.WriteLine("No Play Player remains at same position");
+                   break;
+                case 1:
+                    Console.WriteLine("Player Moves ahead");
+                    break;
+                case 2:
+                    Console.WriteLine("Player moves behind");
+                    break;
 
+            }
         }
     }
 
