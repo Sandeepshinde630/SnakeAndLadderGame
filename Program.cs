c04629943c44fa05e344cc2eccd3ee1cd6a6262c
unknown <sandeepshinde630@gmail.com>
2021-08-05 17:31:53 +0530

    feat:DiceRoll to get no.1-6

--- Program.cs
+++ Program.cs
@@ -8,6 +8,10 @@ namespace SnakeAndLadder
         {
             Console.WriteLine("WelCome to Snake And Ladder Game!");
             int playerreaches = 0;
+            Random rand = new Random();
+            int DiceRoll = rand.Next(1, 7);
+            Console.WriteLine(DiceRoll);
+
         }
     }
 
